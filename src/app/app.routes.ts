b802ca92ce39679154e6fd192970ffb4886f6f4c
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2024-12-28 08:08:54 -0300

    adicionando a funcionalidade condições

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { RacasComponent } from './personagem/racas/racas.component';
 import { OrigensComponent } from './personagem/origens/origens.component';
 import { CalculoDesafiosComponent } from './utilitarios/calculo-desafios/calculo-desafios.component';
+import { CondicoesComponent } from './regras/condicoes/condicoes.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'personagem/racas', component: RacasComponent },
     { path: 'personagem/origem', component: OrigensComponent },
     { path: 'utilitarios/calculo-desafios', component: CalculoDesafiosComponent},
+    { path: 'regras/condicoes', component: CondicoesComponent},
 ];
---
+++ src/app/regras/condicoes/condicoes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-condicoes',
+  imports: [MatDividerModule, MatCardModule],
+  templateUrl: './condicoes.component.html',
+  styleUrl: './condicoes.component.scss'
+})
+export class CondicoesComponent {
+
+}
