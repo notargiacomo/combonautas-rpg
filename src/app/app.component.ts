eb7a30d8a72e5d035f3ef57cbc9bc87f23f5af49
Marcleônio <marcleonio@gmail.com>
2024-12-28 23:40:52 -0300

    loader-indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
+import { NgxLoaderIndicatorDirective } from 'ngx-loader-indicator';
+import { LoadingIndicatorComponent } from './conf/loading-indicator/loading-indicator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatMenuModule, MatButtonModule, MatCardModule, MatDividerModule],
+  imports: [RouterOutlet, MatMenuModule, MatButtonModule, MatCardModule, MatDividerModule, LoadingIndicatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
