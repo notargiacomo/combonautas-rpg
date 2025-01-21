c77183ed821792fea4ee4217b81e0a4b12c18bbd
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-01-21 19:18:45 -0300

    Adicionando filtro traços na Funcionalidade Armas

--- src/app/personagem/itens/itens.component.ts
+++ src/app/personagem/itens/itens.component.ts
@@ -43,7 +43,8 @@ export class ItensComponent {
 
   ngOnInit() {
     this.form = this.fb.group({
-      nome: [],
+      nome: [''],
+      tracos: [''],
     });
 
     this.service.listar({ tipo: TipoItem.ARMA }).subscribe({
@@ -57,19 +58,43 @@ export class ItensComponent {
     });
   }
 
-  consultarArmas() {
-    console.log(this.form.value);
-    let filtro = this.form.value;
+  filtro_traco: string = '';
+
+  consultarArmas(): void {
+    let filtro = { ...this.form.value };
     if (filtro.nome) {
       // regex - in-memory-web-api
       filtro.nome = '^' + filtro.nome;
     }
+
+    if (filtro.tracos) {
+      filtro.tracos = '';
+      this.filtro_traco = this.form.value.tracos;
+    }
+
     this.service.listar(filtro).subscribe({
       next: (response) => {
         this.armas = response;
+        this.numero_registros_armas = this.armas.length;
       },
       error: (response) => {
         console.log(response);
+      },
+      complete: () => {
+        let armas_filtradas: Item[] = [];
+        if(this.filtro_traco.length !== 0){
+          this.armas.forEach((arma) => {
+            arma.tracos?.forEach((traco) => {
+              if(traco.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(this.filtro_traco.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))){
+                if (!armas_filtradas.includes(arma)) {
+                  armas_filtradas.push(arma);
+                }
+              }
+            });
+          });
+          this.armas = armas_filtradas;
+          this.numero_registros_armas = this.armas.length
+        }
       },
     });
   }
