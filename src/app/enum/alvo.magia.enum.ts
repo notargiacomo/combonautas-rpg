06f683847b6c4d74c872cfe69d120f2190d217bd
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-01-31 19:50:27 -0300

    Funcionalidade Magia

--- src/app/enum/alvo.magia.enum.ts
+++ src/app/enum/alvo.magia.enum.ts
@@ -2,6 +2,7 @@ export enum AlvoMagia {
     VOCE = 'Você',
     CRIATURA = 'Criatura',
     CRIATURAS_ESCOLHIDAS = 'Criaturas Escolhidas',
+    CRIATURAS_VOLUNTARIAS = 'Criaturas Voluntárias',
     CRIATURA_OBJETO = 'Criatura ou Objeto',
     AREA_OBJETO = 'Área ou Objeto',
     CRIATURA_OBJETO_AREA = 'Criatura, Área ou Objeto',
