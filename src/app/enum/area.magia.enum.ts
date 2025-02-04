60ed29c2901db0d3b6bb18ff0910907e6388c06a
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-02-04 17:59:39 -0300

    Funcionalidade Magia

---
+++ src/app/enum/area.magia.enum.ts
@@ -0,0 +1,10 @@
+export enum AreaMagia {
+    AREA = 'Área',
+    LINHA = 'Linha',
+    ESFERA = 'Esfera',
+    CONE = 'Cone',
+    CUBO = 'Cubo',
+    QUADRADO = 'Quadrado',
+    CILINDRO = 'Cilindro',
+    CIRCULO = 'Círculo',
+}
