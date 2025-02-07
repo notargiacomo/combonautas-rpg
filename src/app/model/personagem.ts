1fbce7326756e10c37a80f447f80b1a1009f028a
BrunaKQ <brunacbs7@gmail.com>
2025-02-07 20:14:20 -0300

    Adição do Bugigangas e complemento de Personagem.ts

--- src/app/model/personagem.ts
+++ src/app/model/personagem.ts
@@ -11,10 +11,25 @@ export class Personagem {
         for_racial: number;
         for_bonus: number;
         des: number;
+        des_comprada: number;
+        des_racial: number;
+        des_bonus: number;
         con: number;
+        con_comprada: number;
+        con_racial: number;
+        con_bonus: number;
         int: number;
+        int_comprada: number;
+        int_racial: number;
+        int_bonus: number;
         sab: number;
+        sab_comprada: number;
+        sab_racial: number;
+        sab_bonus: number;
         car: number;
+        car_comprada: number;
+        car_racial: number;
+        car_bonus: number;
     };
 
   constructor() {
@@ -37,10 +52,25 @@ export class Personagem {
         for_racial: 0,
         for_bonus: 0,
         des: 0,
+        des_comprada: 0,
+        des_racial: 0,
+        des_bonus: 0,
         con: 0,
+        con_comprada: 0,
+        con_racial: 0,
+        con_bonus: 0,
         int: 0,
+        int_comprada: 0,
+        int_racial: 0,
+        int_bonus: 0,
         sab: 0,
+        sab_comprada: 0,
+        sab_racial: 0,
+        sab_bonus: 0,
         car: 0,
+        car_comprada: 0,
+        car_racial: 0,
+        car_bonus: 0,
       };
     }
   }
