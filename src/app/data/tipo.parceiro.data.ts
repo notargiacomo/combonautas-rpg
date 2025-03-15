061d21d863ddf9a1ea149dae20fdbe09150b0f53
BrunaKQ <brunacbs7@gmail.com>
2025-03-14 22:25:34 -0300

    atualização do Tipo Parceiro

--- src/app/data/tipo.parceiro.data.ts
+++ src/app/data/tipo.parceiro.data.ts
@@ -158,6 +158,162 @@ export class TipoParceiroData {
         referencias: 'Referencia.BASICO',
         pagina: '261'
       },
+      {
+        id: 12,
+        nome: 'Emissário',
+        descricao: 'Um menestrel ou cortesão treinado em apresentações solenes.',
+        tracos: ['AUMENTA', 'PERÍCIA'],
+        parceiro_patamar: [
+          'Iniciante: você recebe +2 em Diplomacia e Intuição.',
+          'Veterano: no início de cada cena, você pode gastar 2 PM para fornecer +2 em testes de perícias baseadas em Carisma para você e seus aliados até o final da cena.',
+          'Mestre: você pode usar o poder Jogo da Corte.'
+        ],
+        referencias: 'Referencia.RUFFGHANOR',
+        pagina: '214'
+      },
+      {
+        id: 13,
+        nome: 'Espião',
+        descricao: 'Um indivíduo discreto e de ouvidos abertos.',
+        tracos: ['AUMENTA', 'PERÍCIA'],
+        parceiro_patamar: [
+          'Iniciante: você recebe +2 em Furtividade e Investigação.',
+          'Veterano: você faz testes de Investigação na metade do tempo e não sofre penalidade em testes de Furtividade por se mover no seu deslocamento normal.',
+          'Mestre: quando faz um teste de Furtividade ou Investigação, você pode gastar 2 PM para rolar dois dados e usar o melhor resultado.'
+        ],
+        referencias: 'Referencia.RUFFGHANOR',
+        pagina: '214'
+      },
+      {
+        id: 14,
+        nome: 'Fortão',
+        descricao: 'Um bárbaro, lutador ou outro tipo que bate primeiro e pensa depois.',
+        tracos: ['AUMENTA', 'DANO'],
+        parceiro_patamar: [
+          'Iniciante: uma vez por rodada, você recebe +1d8 em uma rolagem de dano corpo a corpo.',
+          'Veterano: muda para +1d12.',
+          'Mestre: muda para +3d6.'
+        ],
+        referencias: 'Referencia.RUFFGHANOR',
+        pagina: '214'
+      },
+      {
+        id: 15,
+        nome: 'Guardião',
+        descricao: 'Um cavaleiro, cão de guarda ou outro NPC cuja função primária é proteger.',
+        tracos: ['AUMENTA', 'DEFESA'],
+        parceiro_patamar: [
+          'Iniciante: você recebe +2 na Defesa.',
+          'Veterano: muda para +3.',
+          'Mestre: muda para +4 na Defesa e +2 em testes de resistência.'
+        ],
+        referencias: 'Referencia.BASICO',
+        pagina: '214'
+      },
+      {
+        id: 16,
+        nome: 'Magivocador',
+        descricao: 'Um conjurador especializado em magias ofensivas.',
+        tracos: ['AUMENTA', 'DANO', 'MAGIA', 'CD'],
+        parceiro_patamar: [
+          'Iniciante: o dano de suas magias aumenta em +1 dado do mesmo tipo.',
+          'Veterano: como acima, e a CD para resistir a suas magias aumenta em +1.',
+          'Mestre: como acima, mas dobra os bônus (para um total de +2 dados de dano e +2 na CD).'
+        ],
+        referencias: 'Referencia.BASICO',
+        pagina: '215'
+      },
+      {
+        id: 17,
+        nome: 'Médico',
+        descricao: 'Um clérigo, druida, herbalista ou outro NPC com capacidades curativas.',
+        tracos: ['CURA', 'APOIO','REMOVE CONDIÇÃO'],
+        parceiro_patamar: [
+          'Iniciante: uma vez por rodada você pode gastar 1 PM para curar 1d8+1 PV de uma criatura adjacente.',
+          'Veterano: como acima, mas você pode gastar 3 PM para curar 3d8+3 PV ou remover uma condição prejudicial (como abalado ou fatigado).',
+          'Mestre: como acima, mas você também pode gastar 5 PM para curar 6d8+6 PV.'
+        ],
+        referencias: 'Referencia.BASICO',
+        pagina: '215'
+      },
+      {
+        id: 18,
+        nome: 'Menestrel',
+        descricao: 'Um bardo que acompanha o grupo durante suas aventuras e cria canções e poemas sobre suas aventuras, com a finalidade de aumentar sua fama.',
+        tracos: ['FAMA', 'APOIO'],
+        parceiro_patamar: [
+          'Iniciante: +1 em ajustes de recompensa e 1 ponto de fama bônus sempre que ganha fama.',
+          'Veterano: +1 em ajustes de recompensa e 2 pontos de fama bônus sempre que ganha fama.',
+          'Mestre: +2 em ajustes de recompensa e 3 pontos de fama bônus sempre que ganha fama.'
+        ],
+        referencias: 'Referencia.DB',
+        pagina: 'XX'
+      },
+      {
+        id: 19,
+        nome: 'Mentor',
+        descricao: 'Um notório conhecedor de determinada área, versado na arte de transmitir o que sabe adiante.',
+        tracos: ['PERÍCIA', 'CONHECIMENTO'],
+        parceiro_patamar: [
+          'Iniciante: escolha uma perícia. Você recebe +5 em testes dessa perícia para buscas e +2 em testes dessa perícia para treinamento.',
+          'Veterano: como iniciante, mas você escolhe uma perícia adicional.',
+          'Mestre: como veterano, mas você escolhe uma terceira perícia.'
+        ],
+        referencias: 'Referencia.DB',
+        pagina: 'XX'
+      },
+      {
+        id: 20,
+        nome: 'Místico',
+        descricao: 'Um mago, estudioso ou outro NPC com conhecimento arcano.',
+        tracos: ['MAGIA', 'CONHECIMENTO'],
+        parceiro_patamar: [
+          'Iniciante: o máximo de PM que você pode gastar em suas magias aumenta em +1.',
+          'Veterano: você aprende uma magia adicional de qualquer tipo e círculo que possa lançar.',
+          'Mestre: o máximo de PM que você pode gastar em suas magias aumenta em +1 (para um total de +2) e você aprende uma segunda magia adicional de qualquer tipo e círculo que possa lançar.'
+        ],
+        referencias: 'Referencia.RUFFGHANOR',
+        pagina: '215'
+      },
+      {
+        id: 21,
+        nome: 'Perseguidor',
+        descricao: 'Um caçador, animal farejador ou outro especialista em localizar alvos.',
+        tracos: ['AUMENTA','PERÍCIA','CAÇA'],
+        parceiro_patamar: [
+          'Iniciante: +2 em Percepção e Sobrevivência.',
+          'Veterano: você pode usar Sentidos Aguçados.',
+          'Mestre: você pode usar Percepção às Cegas.'
+        ],
+        referencias: 'Referencia.BASICO',
+        pagina: '261'
+      },
+      {
+        id: 22,
+        nome: 'Sábio',
+        descricao: 'Um estudioso de assuntos diversos.',
+        tracos: ['AUMENTA','PERÍCIA','CONHECIMENTO'],
+        parceiro_patamar: [
+          'Iniciante: você recebe +2 em Conhecimento, Misticismo e Nobreza e pode fazer testes destas perícias mesmo sem ser treinado.',
+          'Veterano: o bônus se aplica a uma perícia adicional, a sua escolha (exceto Luta ou Pontaria).',
+          'Mestre: o bônus muda para +3.'
+        ],
+        referencias: 'Referencia.RUFFGHANOR',
+        pagina: '215'
+      },
+      {
+        id: 23,
+        nome: 'Vigilante',
+        descricao: 'Um vigia ou animal de guarda, sempre atento aos arredores.',
+        tracos: ['PERÍCIA', 'VIGILÂNCIA'],
+        parceiro_patamar: [
+          'Iniciante: +2 em Percepção e Iniciativa.',
+          'Veterano: você pode usar Esquiva Sobrenatural.',
+          'Mestre: você pode usar Olhos nas Costas.'
+        ],
+        referencias: 'Referencia.BASICO',
+        pagina: '215'
+      }
     ];
   }
 }
