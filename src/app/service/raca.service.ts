2a2bfc4aea0f887065c470912ed7c79a1a3a220d
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-04-05 00:35:16 -0300

    História das Raças

--- src/app/service/raca.service.ts
+++ src/app/service/raca.service.ts
@@ -8,31 +8,34 @@ import { AbstractService } from './abstract.service';
 @Injectable({
   providedIn: 'root',
 })
-export class RacaService extends AbstractService{
-
-  constructor(private readonly racaData: RacaData, private readonly http: HttpClient) {
+export class RacaService extends AbstractService {
+  constructor(
+    private readonly racaData: RacaData,
+    private readonly http: HttpClient
+  ) {
     super('raca/');
   }
 
-  listar(filtro:any): Observable<Raca[]> {
-    return this.http.get<Raca[]>(this.url,{params:this.removeBlankAttributes(filtro)})
-    .pipe(map(resultado => {
-      resultado.forEach((raca) => {
-        this.http
-          .get(`assets/doc/${raca.nome_arquivo_descricao}.txt`, {
-            responseType: 'text',
-          })
-          .subscribe((descricao) => (raca.descricao = descricao));
-        // this.http
-        //   .get(`assets/doc/${raca.nome_arquivo_historia}.txt`, {
-        //     responseType: 'text',
-        //   })
-        //   .subscribe((historia) => (raca.historia = historia));
-      });
-  
-      return resultado;
+  listar(filtro: any): Observable<Raca[]> {
+    return this.http
+      .get<Raca[]>(this.url, { params: this.removeBlankAttributes(filtro) })
+      .pipe(
+        map((resultado) => {
+          resultado.forEach((raca) => {
+            this.http
+              .get(`assets/doc/${raca.nome_arquivo_descricao}.txt`, {
+                responseType: 'text',
+              })
+              .subscribe((descricao) => (raca.descricao = descricao));
+            this.http
+              .get(`assets/doc/${raca.nome_arquivo_historia}.txt`, {
+                responseType: 'text',
+              })
+              .subscribe((historia) => (raca.historia = historia));
+          });
 
-    }));
-    
+          return resultado;
+        })
+      );
   }
 }
