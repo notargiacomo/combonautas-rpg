458da98f92aafde189210417d295222c997f66c3
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-10-19 19:49:16 -0300

    arquivo vercel.json

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,9 +1,6 @@
-import { mergeApplicationConfig, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { ApplicationConfig, mergeApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { appConfig } from './app.config';
 
 import { routes } from './app.routes';
 
