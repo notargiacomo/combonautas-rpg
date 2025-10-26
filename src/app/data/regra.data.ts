85300a0b4d78c2451881804bb900892499fd5971
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-10-26 19:11:49 -0300

    funcionalidade regra

--- src/app/data/regra.data.ts
+++ src/app/data/regra.data.ts
@@ -2914,6 +2914,158 @@ export class RegraData {
         `,
         referencia: `Ameaças de Arton - página 224`,
       },
+      {
+        id: 185,
+        nome: `RECURSOS NATURAIS`,
+        descricao: `
+        <p>Certas criaturas, como dragões, elementais e monstros peçonhentos, fornecem recursos específicos como parte de seus tesouros. Entretanto, existem outros materiais de grande utilidade que podem ser extraídos de ameaças. Esta seção apresenta regras opcionais para mestres que desejam usar esse tipo de tesouro em suas aventuras.</p>
+        <p>Recursos naturais podem ser incluídos em qualquer criatura que já não possua um recurso especial, e podem complementar seu tesouro normal ou substituí-lo (a critério do mestre). Para usar um desses recursos, escolha um tipo apropriado à criatura em questão. Alternativamente, você pode rolar na linha referente ao tipo da criatura na Tabela 3-5: Recursos Naturais. A quantidade de recursos fornecida depende do tamanho da criatura; criaturas Pequenas e Médias fornecem 1 unidade do recurso, criaturas Grandes e Enormes fornecem 2 unidades e criaturas Colossais fornecem 5 unidades. Criaturas Minúsculas são muito pequenas para produzir recursos. Embora seja possível extrair recursos de criaturas inteligentes, isso pode ser considerado um tabu, um crime ou simplesmente um ato hediondo, dependendo de onde os aventureiros estiverem</p>
+        <p>Recursos naturais são extraídos como recursos especiais; exigem uma hora de trabalho e um sucesso em um teste de Sobrevivência ou de um Ofício relacionado ao recurso, com CD 15 + ND da criatura. Em caso de falha, os recursos são estragados.</p>
+        <table>
+          <thead>
+            <tr>
+              <th colspan="8">Tabela 3-5: Recursos Naturais</th>
+            </tr>
+            <tr>
+              <th>Tipo de Criatura (1d6)</th>
+              <th>Carapaça</th>
+              <th>Couro</th>
+              <th>Fonte</th>
+              <th>Ingrediente</th>
+              <th>Mantimento</th>
+              <th>Osso</th>
+              <th>Sucata</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Animal</td>
+              <td>1</td>
+              <td>2–3</td>
+              <td>—</td>
+              <td>—</td>
+              <td>4–5</td>
+              <td>6</td>
+              <td>—</td>
+            </tr>
+            <tr>
+              <td>Construto</td>
+              <td>1–2</td>
+              <td>—</td>
+              <td>3</td>
+              <td>—</td>
+              <td>—</td>
+              <td>—</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>Espírito*</td>
+              <td>—</td>
+              <td>—</td>
+              <td>1–3</td>
+              <td>4–6</td>
+              <td>—</td>
+              <td>—</td>
+              <td>—</td>
+            </tr>
+            <tr>
+              <td>Humanoide</td>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+              <td>—</td>
+            </tr>
+            <tr>
+              <td>Monstro</td>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5–6</td>
+              <td>—</td>
+              <td>—</td>
+            </tr>
+            <tr>
+              <td>Morto-vivo</td>
+              <td>1</td>
+              <td>—</td>
+              <td>2–3</td>
+              <td>—</td>
+              <td>—</td>
+              <td>4–6</td>
+              <td>—</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="8">* Use essa linha para criaturas incorpóreas de qualquer tipo.</td>
+            </tr>
+          </tfoot>
+        </table>
+        <br />
+        <h5><p><b>TIPOS DE RECURSOS</b></p></h5>
+        <p>A variedade de recursos existentes em Arton é tão vasta quanto sua fauna. A seguir são descritos os mais comuns, mas você pode expandir esta lista conforme adequado à sua campanha. Use os recursos descritos aqui e nas fichas de ameaças como base para suas ideias.</p>
+        <p>A descrição de cada recurso indica seus usos. Para recursos que podem ser empregados como matéria-prima, seu valor, bem como o grau de qualidade dos itens que podem ser fabricados com ele, depende do patamar da ameaça da qual foi extraído.</p>
+        <table>
+          <thead>
+            <tr>
+              <th colspan="2">Tabela 3-6: Tipos de Recursos Naturais</th>
+            </tr>
+            <tr>
+              <th>Patamar da Ameaça</th>
+              <th>Valor/Qualidade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Iniciante</td>
+              <td>T$ 20 para itens normais</td>
+            </tr>
+            <tr>
+              <td>Veterano</td>
+              <td>T$ 100 para itens superiores</td>
+            </tr>
+            <tr>
+              <td>Campeão</td>
+              <td>T$ 500 para itens mágicos</td>
+            </tr>
+            <tr>
+              <td>Lenda</td>
+              <td>T$ 2.500 para itens mágicos</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <p><b>Carapaça.</b> Diversos tipos de carapaças servem como matéria-prima para couraças, lorigas segmentadas, meias armaduras, armaduras completas e escudos pesados. Esses itens contam como itens naturais para devotos de Allihanna, mas são mais frágeis que metal; cada vez que você sofre um acerto crítico, há 10% (1 em 1d10) de chance de que o item seja destruído. Um item mágico ou feito inteiramente de peças de uma criatura lenda não sofre esse efeito.</p>
+        <p><b>Couro.</b> Serve como matéria-prima para armaduras acolchoadas, armaduras de couro, gibões de peles e escudos leves. Ao fabricar uma versão mágica desses itens, você pode gastar 10 peças de couro de lendas para reduzir o custo do item em –1 PM.</p>
+        <p><b>Fonte.</b> Das penas de uma fênix às presas de um vampiro, várias criaturas carregam uma fagulha mística que pode ser usada como matéria-prima de itens esotéricos. Além disso, ao fabricar um acessório mágico, você pode gastar 10 fontes de lendas para reduzir o custo do item em –1 PM.</p>
+        <p><b>Ingrediente.</b> Diversas criaturas produzem substâncias que podem ser empregadas como ingredientes para itens alquímicos e poções mágicas, independente de seu patamar.</p>
+        <p><b>Mantimento.</b> Carne, leite, ovos e outros materiais podem ser usados como alimentos. Cada porção alimenta quatro pessoas; mantimentos de uma criatura iniciante contam como uma refeição comum, de uma criatura veterana ou campeã contam como um prato especial (determinado pelo mestre) e de uma criatura lenda contam como dois pratos especiais (com efeitos cumulativos).</p>
+        <p><b>Osso.</b> Ossos das mais variadas partes de criaturas servem como matéria-prima de armas. Peças de patamar veterano também podem ser usadas nas melhorias espinhoso ou macabro. Além disso, ao fabricar uma maça do terror ou um item tumular ou assustador, você pode pode gastar 10 peças de osso de lendas para reduzir o custo do item em –1 PM.</p>
+        <p><b>Sucata.</b> Partes de golens e outras criaturas artificiais servem como matéria-prima de engenhocas e itens de metal. Além disso, quando fabrica uma engenhoca, você pode gastar uma peça de sucata para receber um bônus no teste de Ofício (cumulativo com bônus de outros itens), conforme o patamar da criatura: veterano, +2; campeão, +5; lenda, +10.</p>
+        
+        `,
+        referencia: `Ameaças de Arton - páginas 401 e 402`,
+      },
+      {
+        id: 186,
+        nome: `PARCEIROS ESPECIAIS`,
+        descricao: `
+        <p>Um parceiro especial fornece os benefícios listados em sua descrição, mas conta como o tipo indicado para habilidades que fornecem um parceiro desse tipo. Por exemplo, uma hiena (um parceiro especial perseguidor) pode ser escolhida por um personagem com o poder Companheiro Animal (que permite escolher um parceiro perseguidor). Alguns parceiros possuem mais de um tipo; nesse caso, seu tipo é definido quando ele é criado e não pode ser alterado posteriormente.</p>
+        `,
+        referencia: `Ameaças de Arton - página 416`,
+      },
+      {
+        id: 187,
+        nome: `NATAÇÃO E PARCEIROS`,
+        descricao: `
+        <p>Exceto quando indicado em sua descrição, parceiros que fornecem deslocamento de natação não permitem ao personagem falar ou respirar debaixo d’água nem anulam as penalidades padrão por estar submerso.</p>
+        `,
+        referencia: `Ameaças de Arton - página 416`,
+      },
     ];
   }
 }
