767fe677f991cab173e8ce887e4472f8dcd7cf7d
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-11-03 11:06:12 -0300

    Geração de Tabela de DInheiro Funcional

--- src/app/controller/mestre/tabela-geracao-tesouros/tabela-geracao-tesouros.component.ts
+++ src/app/controller/mestre/tabela-geracao-tesouros/tabela-geracao-tesouros.component.ts
@@ -25,7 +25,9 @@ export class TabelaGeracaoTesourosComponent {
   tabelaTesouro: any[] = [];
   tipos: string[] = ['PADRÃO', 'METADE', 'DOBRO'];
   niveis: string[] = ['1/4', '1/2', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
-  tesouro: string = '';
+  tesouroDinheiro: string = '';
+  tesouroItens: string = '';
+  tesouro:string = ';'
 
 
   constructor(private fb: FormBuilder,
@@ -57,43 +59,53 @@ export class TabelaGeracaoTesourosComponent {
     // Busca o objeto cujo intervalo [min, max] contém o valor
     const tabelaDinheiroNivel = tabelaTesouroDinheiro.filter((item: { nd: string }) => item.nd === nivel);
     const linhaDinheiroNivel = tabelaDinheiroNivel.find((item: { min: number; max: number; }) => randomDinheiro >= item.min && randomDinheiro <= item.max);
+    
+    
+    let dinheiro = '0';
+    if(linhaDinheiroNivel.unidade.includes("riqueza")){
+      dinheiro = "T$ " + this.calcularRiqueza(linhaDinheiroNivel.unidade, linhaDinheiroNivel.modificador>0);
+    } else {
+
+      let valorDinheiro: number = 
+            this.formulario.get('tipo')?.value === 'METADE' ? this.contadorMoedas(linhaDinheiroNivel)/2: 
+            this.formulario.get('tipo')?.value === 'DOBRO'? this.contadorMoedas(linhaDinheiroNivel)*2: 
+            this.contadorMoedas(linhaDinheiroNivel);
+
+      dinheiro = linhaDinheiroNivel.unidade + " " + valorDinheiro;
+    }
+
+    this.tesouroDinheiro = "RESULTADO D100: " + randomDinheiro + " / FÓRMULA: " + linhaDinheiroNivel.dinheiro + " / VALORES: " + dinheiro;
+  }
+
+  calcularRiqueza(unidade:string, modificador: boolean): string {
+    let riquezas = []
+    if(unidade.includes("menor"))
+      console.log("entrou riqueza menor");
+      riquezas = this.tabelaTesouro[1];
+    if(unidade.includes("media"))
+      riquezas = this.tabelaTesouro[2];
+    if(unidade.includes("maior"))
+      riquezas = this.tabelaTesouro[3];
+    
+    let random = Math.floor(Math.random() * 100)+1;
+    const linhaDinheiroNivel = riquezas.find((item: { min: number; max: number; }) => random >= item.min && random <= item.max);
+    
+    return this.contadorMoedas(linhaDinheiroNivel).toString();
+  }
+
+
+  contadorMoedas(linhaDinheiroNivel: any): number {
     let dinheiro = 0;
-
     for (let index = 0; index < linhaDinheiroNivel.multiplicador; index++) {
-      dinheiro += Math.floor(Math.random() * linhaDinheiroNivel.randomizador)+1;
+      dinheiro += Number(Math.floor(Math.random() * linhaDinheiroNivel.randomizador) + 1);
     }
 
-    let unidade = linhaDinheiroNivel.unidade;
-    if(linhaDinheiroNivel.unidade.includes("riqueza menor")){
-      unidade = this.calcularRiquezaMenor(linhaDinheiroNivel.modificador>0);
+    if (linhaDinheiroNivel.parcela) {
+      dinheiro += Number(linhaDinheiroNivel.parcela);
     }
-
-    if(linhaDinheiroNivel.unidade.includes("riqueza média")){
-      unidade = this.calcularRiquezaMedia(linhaDinheiroNivel.modificador>0);
-    }
-
-    if(linhaDinheiroNivel.unidade.includes("riqueza maior")){
-      unidade = this.calcularRiquezaMaior(linhaDinheiroNivel.modificador>0);
-    }
-
-    dinheiro += linhaDinheiroNivel.parcela;
-    dinheiro = dinheiro*linhaDinheiroNivel.potencializador;
-    this.tesouro = "RESULTADO D100: " + randomDinheiro + " / FÓRMULA: " + linhaDinheiroNivel.dinheiro + " / DINHEIRO: " + (!linhaDinheiroNivel.unidade.includes("riqueza") ? linhaDinheiroNivel.unidade + " " + dinheiro.toString() : unidade );
+    dinheiro = dinheiro * linhaDinheiroNivel.potencializador;
+    return dinheiro;
   }
 
-  calcularRiquezaMenor(modificador: boolean): string {
-    let riquezasMenores = this.tabelaTesouro[1];
-    return ''
-  }
-
-  calcularRiquezaMedia(modificador: boolean): string {
-    let riquezasMedias = this.tabelaTesouro[2];
-    return ''
-  }
-
-  calcularRiquezaMaior(modificador: boolean): string {
-    let riquezasMaiores = this.tabelaTesouro[3];
-    return ''
-  }
 
 }
