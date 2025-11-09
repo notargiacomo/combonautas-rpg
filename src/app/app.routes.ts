1f173191673a6f54fedd4d84494968c95c165db3
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-11-09 10:23:40 -0300

    padronizando layout padrão do body da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,16 @@ import { RacasComponent } from './controller/personagem/racas/racas.component';
 import { AcoesComponent } from './controller/regras/acoes/acoes.component';
 import { CondicoesComponent } from './controller/regras/condicoes/condicoes.component';
 import { RegrasComponent } from './controller/regras/regras.component';
-import { CalculoDesafiosComponent } from './controller/utilitarios/calculo-desafios/calculo-desafios.component';
+import { CalculoDesafiosComponent } from './controller/mestre/calculo-desafios/calculo-desafios.component';
 import { PersonagemAleatorioComponent } from './controller/utilitarios/personagem-aleatorio/personagem-aleatorio.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'regras/condicoes', component: CondicoesComponent},
-    { path: 'regras/acoes', component: AcoesComponent},
-    { path: 'regras/regras', component: RegrasComponent},
-    { path: 'mestre/calculo-desafios', component: CalculoDesafiosComponent},
-    { path: 'mestre/tabela-geracao-tesouros', component: TabelaGeracaoTesourosComponent},
-    { path: 'utilitarios/personagem-aleatorio', component: PersonagemAleatorioComponent},
-    { path: 'personagem/racas', component: RacasComponent},
+    { path: 'regras/condicoes', component: CondicoesComponent },
+    { path: 'regras/acoes', component: AcoesComponent },
+    { path: 'regras/regras', component: RegrasComponent },
+    { path: 'mestre/calculo-desafios', component: CalculoDesafiosComponent },
+    { path: 'mestre/tabela-geracao-tesouros', component: TabelaGeracaoTesourosComponent },
+    { path: 'utilitarios/personagem-aleatorio', component: PersonagemAleatorioComponent },
+    { path: 'personagem/racas', component: RacasComponent },
 ];
--- src/app/controller/mestre/calculo-desafios/calculo-desafios.component.ts
+++ src/app/controller/mestre/calculo-desafios/calculo-desafios.component.ts
@@ -1,11 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -23,15 +17,29 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-import { ameacas } from '@app/data/ameacas.data'; 
-import { CalculoDesafiosDto } from '@app/dto/calculo-desafios.dto'; 
+import { ameacas } from '@app/data/ameacas.data';
+import { CalculoDesafiosDto } from '@app/dto/calculo-desafios.dto';
 import { Ameaca } from '@app/model/ameaca';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-calculo-desafios',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, ReactiveFormsModule, MatDividerModule, MatCardModule, MatInputModule, MatCheckboxModule, MatRadioModule, MatSelectModule, MatListModule,MatIconModule, MatButtonModule, MatExpansionModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+    MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calculo-desafios.component.html',
   styleUrl: './calculo-desafios.component.scss',
@@ -44,9 +52,9 @@ export class CalculoDesafiosComponent implements OnInit {
 
   situacoes: string[] = ['FAVORÁVEL', 'NEUTRA', 'DESFAVORÁVEL'];
 
-  monstros:Ameaca[] = ameacas
+  monstros: Ameaca[] = ameacas;
 
-  monstrosSelecionadosAleatoriamente: any[] = []
+  monstrosSelecionadosAleatoriamente: any[] = [];
 
   constructor(private fb: FormBuilder) {}
 
@@ -54,57 +62,53 @@ export class CalculoDesafiosComponent implements OnInit {
     this.dialog.open(DialogElementsExampleDialog);
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.formulario = this.fb.group({
       nivel: new FormControl(this.calculoDesafios.nivel),
       numero_jogadores: new FormControl(this.calculoDesafios.numero_jogadores),
       experientes: new FormControl(this.calculoDesafios.experientes),
       entrosados: new FormControl(this.calculoDesafios.entrosados),
       situacao: new FormControl(this.calculoDesafios.situacao),
-      numero_encontros: new FormControl(this.calculoDesafios.numero_encontros)
+      numero_encontros: new FormControl(this.calculoDesafios.numero_encontros),
     });
     this.calcular();
   }
 
-  calcular(){
+  calcular() {
     // console.log(this.formulario.get("entrosados")?.value);
-    const valores : CalculoDesafiosDto = this.formulario.value;
+    const valores: CalculoDesafiosDto = this.formulario.value;
     var resultado: number = valores.nivel;
     resultado += valores.numero_jogadores - 4;
     resultado += valores.experientes ? 1 : 0;
     resultado += valores.entrosados ? 1 : 0;
     resultado += valores.situacao;
-    resultado += -(valores.numero_encontros-1);
-    this.listaResultado(resultado)
-    this.resultado = valores.numero_encontros + " ENCONTRO(S) DE ND " + resultado;
-
+    resultado += -(valores.numero_encontros - 1);
+    this.listaResultado(resultado);
+    this.resultado = valores.numero_encontros + ' ENCONTRO(S) DE ND ' + resultado;
   }
 
-  listaResultado(nd: number){
-    this.monstrosSelecionadosAleatoriamente = []
+  listaResultado(nd: number) {
+    this.monstrosSelecionadosAleatoriamente = [];
 
     let shuffled: Ameaca[] = this.monstros
-    .map((value:Ameaca) => ({ value, sort: Math.random() }))
-    .sort((a:any, b:any) => a.sort - b.sort)
-    .map((value:any) => value.value)
+      .map((value: Ameaca) => ({ value, sort: Math.random() }))
+      .sort((a: any, b: any) => a.sort - b.sort)
+      .map((value: any) => value.value);
 
-    this.encontrarMonstros (nd,shuffled);
+    this.encontrarMonstros(nd, shuffled);
   }
 
-  encontrarMonstros(nd: number, lista: Ameaca[], soma:number=0, posicao:number = 0){
-
+  encontrarMonstros(nd: number, lista: Ameaca[], soma: number = 0, posicao: number = 0) {
     for (let index = posicao; index < lista.length; index++) {
       const element = lista[index];
-      
+
       if (soma + element.nd <= nd) {
         this.monstrosSelecionadosAleatoriamente.push(element);
-        this.encontrarMonstros (nd, lista, soma + element.nd, index++, );
-        break
+        this.encontrarMonstros(nd, lista, soma + element.nd, index++);
+        break;
       }
-      
     }
   }
-
 }
 
 @Component({
