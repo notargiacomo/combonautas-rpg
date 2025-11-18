ca6db9371c0da2c21c95f7c967fd077494ab72fe
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2025-11-18 15:08:39 -0300

    menu armadura & escudo

--- src/app/controller/itens/armaduras-escudos/armaduras-escudos.component.ts
+++ src/app/controller/itens/armaduras-escudos/armaduras-escudos.component.ts
@@ -1,11 +1,20 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CardSearchComponent } from '@app/components/card-search/card-search.component';
+import { CombonautasBase } from '@app/components/combonautas-base';
+import { ArmadurasEscudosService } from '@app/service/armaduras.escudos.service';
 
 @Component({
   selector: 'app-armaduras-escudos',
-  imports: [],
+  standalone: true,
+  imports: [CardSearchComponent, MatCard, MatCardTitle, MatCardContent, NgFor, MatTabGroup, MatTab, NgIf],
   templateUrl: './armaduras-escudos.component.html',
-  styleUrl: './armaduras-escudos.component.scss'
+  styleUrl: './armaduras-escudos.component.scss',
 })
-export class ArmadurasEscudosComponent {
-
+export class ArmadurasEscudosComponent extends CombonautasBase {
+  constructor(readonly service: ArmadurasEscudosService) {
+    super();
+  }
 }
