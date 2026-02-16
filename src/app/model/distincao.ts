1aa947d84fb3221404688d0ec0ab378de18a971d
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2026-02-16 01:05:49 -0300

    reforma na funcionalidade dinstinção

--- src/app/model/distincao.ts
+++ src/app/model/distincao.ts
@@ -1,20 +1,15 @@
-import { Poder } from "./poder";
+import { Poder } from './poder';
 
 export interface Distincao {
-
   id: number;
   nome?: string;
-  nome_arquivo_descricao?: string;
-  nome_arquivo_informacoes?: string;
-  nome_arquivo_admissao?: string;
-  admissao?:string;
-  marca_distincao?:string;
+  admissao?: string;
+  marca_distincao?: string;
   poderes?: Poder[];
-  descricao_distincao?: string;
+  descricao?: string;
   ha_informacoes_adicionais?: boolean;
-  descricao_informacoes?:string;
-  imagem?: string,
-  referencias?: string,
-  paginas?: string
-
+  descricao_informacoes?: string;
+  imagem?: string;
+  referencias?: string;
+  paginas?: string;
 }
--- src/app/service/distincao.service.ts
+++ src/app/service/distincao.service.ts
@@ -13,30 +13,7 @@ export class DistincaoService extends AbstractService {
   }
 
   listar(filtro: any): Observable<Distincao[]> {
-    return this.http.get<Distincao[]>(this.url, { params: this.removeBlankAttributes(filtro) }).pipe(
-      map(resultado => {
-        resultado.forEach(distincao => {
-          this.http
-            .get(`assets/doc/${distincao.nome_arquivo_descricao}.txt`, {
-              responseType: 'text',
-            })
-            .subscribe(descricao_distincao => (distincao.descricao_distincao = descricao_distincao));
-          this.http
-            .get(`assets/doc/${distincao.nome_arquivo_admissao}.txt`, {
-              responseType: 'text',
-            })
-            .subscribe(admissao => (distincao.admissao = admissao));
-          if (distincao.ha_informacoes_adicionais) {
-            this.http
-              .get(`assets/doc/${distincao.nome_arquivo_informacoes}.txt`, {
-                responseType: 'text',
-              })
-              .subscribe(descricao_informacoes => (distincao.descricao_informacoes = descricao_informacoes));
-          }
-        });
-        return resultado;
-      })
-    );
+    return this.http.get<Distincao[]>(this.url, { params: this.removeBlankAttributes(filtro) });
   }
 
   getbyId(id: number | undefined) {
@@ -44,19 +21,7 @@ export class DistincaoService extends AbstractService {
   }
 
   consult(filtro: any): Observable<Distincao[]> {
-    let listas = this.http.get<Distincao[]>(this.url).pipe(
-      switchMap((distincoes: Distincao[]) => {
-        const requests = distincoes.map(distincao =>
-          this.http.get(`assets/doc/${distincao.nome_arquivo_descricao}.txt`, { responseType: 'text' }).pipe(
-            map(descricao_distincao => ({
-              ...distincao,
-              descricao_distincao,
-            }))
-          )
-        );
-        return forkJoin(requests);
-      })
-    );
-    return this.filtrar(filtro, listas, ['nome', 'tipo', 'tamanho', 'deslocamentos', 'sentidos', 'descricao']);
+    let listas = this.http.get<Distincao[]>(this.url);
+    return this.filtrar(filtro, listas, ['nome']);
   }
 }
