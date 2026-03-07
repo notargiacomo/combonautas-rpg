c39964d94157048940655055454662155ee82b27
Flávio Ramos <flavio.notargiacomo.ramos@gmail.com>
2026-03-06 22:00:39 -0300

    ajustes

--- src/app/dao/acessos.dao.ts
+++ src/app/dao/acessos.dao.ts
@@ -13,23 +13,22 @@ export class Acesso extends GenericRepository<AcessoSb> {
   async buscar() {
     const { data, error } = await this.client.from('tb_acesso').select('total').eq('id', 1).single();
 
-    return data!.total;
+    if (error) {
+      console.error('Erro ao buscar acessos:', error);
+      throw error;
+    }
+
+    return data?.total ?? 0;
   }
 
   async atualizar() {
-    const totalAtual = await this.buscar();
-
-    const { data, error } = await this.client
-      .from('tb_acesso')
-      .update({ total: totalAtual + 1 })
-      .eq('id', 1)
-      .select('total')
-      .single();
+    const { data, error } = await this.client.rpc('incrementar_acesso');
 
     if (error) {
-      console.error('Erro ao inserir regra:', error);
+      console.error('Erro ao incrementar acessos:', error);
       throw error;
     }
-    return data.total;
+
+    return data;
   }
 }
